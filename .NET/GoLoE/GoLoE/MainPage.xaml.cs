187e5edb67398ed674038836a900938f2a334335
Nuzul Abatony <albtony@gmail.com>
2023-02-20 00:16:10 +0700

    commit finished simple .NET app

---
+++ .NET/GoLoE/GoLoE/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace GoLoE;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void onCompare(object sender, EventArgs e)
+	{
+		int leftNumber = int.Parse(InputLeftNumber.Text);
+		int rightNumber = int.Parse(InputRightNumber.Text);
+
+		string status = "equal to";
+
+		if(leftNumber > rightNumber)
+		{
+			status = "greater than";
+		} else if(leftNumber < rightNumber)
+		{
+			status = "lower than";
+		} 
+
+		ResultLabel.Text = String.Format("{0} is {1} {2}!", rightNumber, status, leftNumber);
+	}
+}
+
