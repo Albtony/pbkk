be704b5b05768919094c4574a97674c644c4efff
Nuzul Abatony <albtony@gmail.com>
2023-03-27 08:13:48 +0700

    feat: finish model for contact

---
+++ .NET/Contact Database/Models/contact.cs
@@ -0,0 +1,23 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contact_Database.Models
+{
+    [Table("contactList")]
+    public class Contact
+    {
+        [PrimaryKey]
+        public int Id { get; set; }
+
+        public String ContactName { get; set; }
+
+        public String InstituteName { get; set; }
+        public String Email { get; set; }
+        public String InstitutePhoneNo { get; set; }
+        public String PrivatePhoneNo { get; set; }
+    }
+}
