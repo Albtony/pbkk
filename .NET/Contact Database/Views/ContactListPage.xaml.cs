1a00334dab1aa7662f31400e077a5eaa5278ae50
Nuzul Abatony <albtony@gmail.com>
2023-03-27 08:09:03 +0700

    feat: finish view contacts UI

---
+++ .NET/Contact Database/Views/ContactListPage.xaml.cs
@@ -0,0 +1,18 @@
+using Contact_Database.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+
+namespace Contact_Database.Views;
+
+public partial class ContactListPage : ContentPage
+{
+    public ContactListPage()
+    {
+        InitializeComponent();
+    }
+    async void OnGetContactClicked(object sender, EventArgs args)
+    {
+        //TODO: implement
+    }
+}
