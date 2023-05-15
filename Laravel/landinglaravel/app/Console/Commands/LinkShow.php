ab977aa371b7c0dbf4c1956d05349b70534ec0d3
Nuzul Abatony <albtony@gmail.com>
2023-05-15 09:52:42 +0700

    finish eloquent tutorial 2

--- Laravel/landinglaravel/app/Console/Commands/LinkShow.php
+++ Laravel/landinglaravel/app/Console/Commands/LinkShow.php
@@ -5,7 +5,7 @@ namespace App\Console\Commands;
 use App\Models\Link;
 use Illuminate\Console\Command;
 
-class LinkList extends Command
+class LinkShow extends Command
 {
     /**
      * The name and signature of the console command.
@@ -38,9 +38,15 @@ class LinkList extends Command
      */
     public function handle()
     {
-        $headers = [ 'id', 'url', 'description' ];
-        $links = Link::all(['id', 'url', 'description'])->toArray();
-        $this->table($headers, $links);
+        $headers = [ 'id', 'url', 'list', 'description' ];
+        $links = Link::all();
+
+        $table_rows = [];
+        foreach ($links as $link) {
+            $table_rows[] = [ $link->id, $link->url, $link->link_list->slug, $link->description ];
+        }
+
+        $this->table($headers, $table_rows);
 
         return 0;
     }
