34b8ec1924faf7d2e83d64d63a27fd7d11bc9e03
Nuzul Abatony <albtony@gmail.com>
2023-05-29 09:31:46 +0700

    fix: wrong table structure and missing api points

--- Laravel/laravel-api/routes/api.php
+++ Laravel/laravel-api/routes/api.php
@@ -17,7 +17,10 @@ use App\Http\Controllers\API\ProductController;
 |
 */
   
-  
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
 Route::controller(RegisterController::class)->group(function(){
     Route::post('register', 'register');
     Route::post('login', 'login');
